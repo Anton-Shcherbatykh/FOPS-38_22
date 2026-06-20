9c00d7faa258ec644636b3d7b9d7c2e5599b1966
Anton <anton.shcherbatykh.2000@gmail.com>
2026-06-20 04:18:30 +0300

    Update storage.tf

--- 22-02/Files/storage.tf
+++ 22-02/Files/storage.tf
@@ -1,30 +1,36 @@
-# Сервисный аккаунт для доступа к Object Storage
 resource "yandex_iam_service_account" "storage_sa" {
   name        = "storage-sa"
-  description = "Service account for Object Storage"
   folder_id   = var.yc_folder_id
 }
 
-# Статический ключ доступа для сервисного аккаунта
+resource "yandex_resourcemanager_folder_iam_member" "storage_editor" {
+  folder_id = var.yc_folder_id
+  role      = "storage.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.storage_sa.id}"
+}
+
 resource "yandex_iam_service_account_static_access_key" "storage_key" {
   service_account_id = yandex_iam_service_account.storage_sa.id
+  depends_on         = [yandex_resourcemanager_folder_iam_member.storage_editor]
 }
 
-# Бакет Object Storage (публичный)
-resource "yandex_storage_bucket" "pictures" {
-  bucket     = var.bucket_name
-  acl        = "public-read"
-  access_key = yandex_iam_service_account_static_access_key.storage_key.access_key
-  secret_key = yandex_iam_service_account_static_access_key.storage_key.secret_key
+resource "yandex_storage_bucket" "bucket" {
+  bucket      = var.bucket_name
+  access_key  = yandex_iam_service_account_static_access_key.storage_key.access_key
+  secret_key  = yandex_iam_service_account_static_access_key.storage_key.secret_key
+  folder_id   = var.yc_folder_id
+  anonymous_access_flags {
+    read = true
+  }
+  depends_on = [yandex_resourcemanager_folder_iam_member.storage_editor]
 }
 
-# Загрузка картинки в бакет
 resource "yandex_storage_object" "image" {
-  bucket       = yandex_storage_bucket.pictures.bucket
-  key          = "shared-image.jpg"   # имя файла в бакете
+  bucket       = yandex_storage_bucket.bucket.bucket
+  key          = "mi24.jpg"
   source       = var.image_local_path
+  content_type = "image/jpeg"
   access_key   = yandex_iam_service_account_static_access_key.storage_key.access_key
   secret_key   = yandex_iam_service_account_static_access_key.storage_key.secret_key
-  acl          = "public-read"
-  depends_on   = [yandex_storage_bucket.pictures]
+  depends_on   = [yandex_storage_bucket.bucket]
 }

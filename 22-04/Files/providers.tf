39b422fad019138c07265a41f01b78b23ce3cd03
Anton <anton.shcherbatykh.2000@gmail.com>
2026-07-02 01:44:13 +0300

    Update providers.tf

--- 22-04/Files/providers.tf
+++ 22-04/Files/providers.tf
@@ -3,14 +3,16 @@ terraform {
     yandex = {
       source = "yandex-cloud/yandex"
     }
+    random = {
+      source = "hashicorp/random"
+    }
   }
   required_version = ">= 0.13"
 }
 
-# Конфигурация провайдера Yandex Cloud
 provider "yandex" {
-  token     = var.yc_token     # OAuth-токен (из переменных)
-  cloud_id  = var.yc_cloud_id  # ID облака
-  folder_id = var.yc_folder_id # ID каталога
-  zone      = var.zone         # Зона для ресурсов
+  token     = var.yc_token
+  cloud_id  = var.yc_cloud_id
+  folder_id = var.yc_folder_id
+  zone      = var.zone
 }
